8036dabbc5cfa487c6f8cd64b8e2c758cd8d1000
Breno Duarte <brenoduarte239@gmail.com>
2024-05-29 17:13:25 -0300

    Update Bootcamp.java

--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -7,64 +7,73 @@ import java.util.Objects;
 import java.util.Set;
 
 public class Bootcamp {
-    private String nome;
-    private String descricao;
-    private final LocalDate dataInicial = LocalDate.now();
-    private final LocalDate dataFinal = dataInicial.plusDays(45);
-    private Set<Dev> devsInscritos = new HashSet<>();
-    private Set<Conteudo> conteudos = new LinkedHashSet<>();
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public LocalDate getDataInicial() {
-        return dataInicial;
-    }
-
-    public LocalDate getDataFinal() {
-        return dataFinal;
-    }
-
-    public Set<Dev> getDevsInscritos() {
-        return devsInscritos;
-    }
-
-    public void setDevsInscritos(Set<Dev> devsInscritos) {
-        this.devsInscritos = devsInscritos;
-    }
-
-    public Set<Conteudo> getConteudos() {
-        return conteudos;
-    }
-
-    public void setConteudos(Set<Conteudo> conteudos) {
-        this.conteudos = conteudos;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Bootcamp bootcamp = (Bootcamp) o;
-        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsInscritos, bootcamp.devsInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsInscritos, conteudos);
-    }
+	private String nome;
+	private String descricao;
+	private LocalDate dataInicio = LocalDate.now();
+	private final LocalDate dataFinal = dataInicio.plusDays(45);
+	private Set<Dev> devsInscritos = new HashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+	
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	
+	public Set<Dev> getDevsInscritos() {
+		return devsInscritos;
+	}
+	
+	public void setDevsInscritos(Set<Dev> devsInscritos) {
+		this.devsInscritos = devsInscritos;
+	}
+	
+	public Set<Conteudo> getConteudos() {
+		return conteudos;
+	}
+	
+	public void setConteudos(Set<Conteudo> conteudos) {
+		this.conteudos = conteudos;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudos, dataFinal, dataInicio, descricao, devsInscritos, nome);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp other = (Bootcamp) obj;
+		return Objects.equals(conteudos, other.conteudos) && Objects.equals(dataFinal, other.dataFinal)
+				&& Objects.equals(dataInicio, other.dataInicio) && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(devsInscritos, other.devsInscritos) && Objects.equals(nome, other.nome);
+	}
+	
+	
+	
 }
