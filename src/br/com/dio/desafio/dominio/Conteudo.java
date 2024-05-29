a24df3e376e3c928e214bc7ba4243ad6fa81d569
Breno Duarte <brenoduarte239@gmail.com>
2024-05-29 17:13:49 -0300

    Update Conteudo.java

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,27 +1,26 @@
 package br.com.dio.desafio.dominio;
 
 public abstract class Conteudo {
+	private String titulo;
+	private String descricao;
+	protected static final double XP_PADRAO = 10; 
+	
+	public abstract double calcularXp();
 
-    protected static final double XP_PADRAO = 10d;
+	public String getTitulo() {
+		return titulo;
+	}
 
-    private String titulo;
-    private String descricao;
+	public void setTitulo(String titutlo) {
+		this.titulo = titutlo;
+	}
 
-    public abstract double calcularXp();
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 }
